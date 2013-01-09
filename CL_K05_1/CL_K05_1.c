b34ea75356c046315e0193788539da4936e66210
masudatokimi <bctm1220223@gn.iwasaki.ac.jp>
2013-01-09 10:34:10 +0900

    1回目
    5回以上回す

---
+++ CL_K05_1/CL_K05_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*sakuseisya:1T2(40) masudatokimi*/
+/*kdaimei:kadai05_1*/
+struct data {
+	char name[256];
+	double height;
+	double weight;
+};
+
+void main(void) {
+	struct data sd = {"iwasakidejita",175.5,73.2};
+	printf("sokuteikextuka:\n");
+	printf("%s  %fcmA%fkg\n",sd.name,sd.height,sd.weight);
+}
